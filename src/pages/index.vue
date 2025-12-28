2c1d185000f672353a2a981468d9a08f40e856f2
CWorld <cworld0@qq.com>
2025-12-28 20:59:34 +0800

    feat(pages): init minesweeper

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,46 +1,124 @@
-<script setup lang="ts" generic="T extends any, O extends any">
-defineOptions({
-  name: 'IndexPage',
-})
+<script setup lang="ts">
+import { reactive } from "vue";
 
-const name = ref('')
-
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
+interface BlockState {
+  x: number;
+  y: number;
+  revealed: boolean;
+  mine: boolean;
+  flagged: boolean;
+  adjacentMines: number;
 }
+
+const WIDTH = 10;
+const HEIGHT = 10;
+
+const state = reactive<BlockState[][]>(
+  Array.from({ length: HEIGHT }, (_, y) =>
+    Array.from(
+      { length: WIDTH },
+      (_, x): BlockState => ({
+        x,
+        y,
+        revealed: false,
+        mine: false,
+        flagged: false,
+        adjacentMines: 0,
+      }),
+    ),
+  ),
+);
+
+function generateMines() {
+  for (const row of state) {
+    for (const block of row) {
+      block.mine = Math.random() < 0.3;
+      block.revealed = false;
+      block.flagged = false;
+      block.adjacentMines = 0;
+    }
+  }
+}
+
+function onClick(x: number, y: number) {
+  console.log(`Clicked at ${x}, ${y}`);
+}
+
+const directions = [
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+];
+
+const numberColors = [
+  "text-transparent",
+  "text-blue-500",
+  "text-green-500",
+  "text-red-500",
+  "text-purple-500",
+  "text-yellow-500",
+  "text-orange-500",
+  "text-pink-500",
+];
+
+function updateNumbers() {
+  state.forEach((row, y) => {
+    row.forEach((block, x) => {
+      if (block.mine) return;
+      block.adjacentMines = 0;
+      directions.forEach(([dx, dy]) => {
+        const x2 = x + dx;
+        const y2 = y + dy;
+
+        if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT) return;
+        if (state[y2][x2].mine) block.adjacentMines += 1;
+      });
+    });
+  });
+}
+
+function getBlockClass(block: BlockState) {
+  return block.mine ? "bg-red-500/10 text-red-200" : "text-grey";
+}
+
+generateMines();
+updateNumbers();
 </script>
 
 <template>
   <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu-collective/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
+    Minesweeper
 
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-
-    <div>
-      <button
-        class="text-sm m-3 btn"
-        :disabled="!name"
-        @click="go"
+    <div p5>
+      <div
+        v-for="(row, y) in state"
+        :key="y"
+        flex="~"
+        items-center
+        justify-center
       >
-        Go
-      </button>
+        <button
+          v-for="(item, x) in row"
+          :key="x"
+          flex="~"
+          items-center
+          justify-center
+          w-10
+          h-10
+          border="1 gray-300/30"
+          hover="bg-gray/30"
+          :class="getBlockClass(item)"
+          @click="onClick(x, y)"
+        >
+          <div v-if="item.mine" i-mdi-mine />
+          <div v-else>{{ item.adjacentMines }}</div>
+        </button>
+      </div>
     </div>
   </div>
 </template>
